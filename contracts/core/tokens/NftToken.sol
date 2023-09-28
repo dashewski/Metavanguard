a56693b431fd9d1028e9d408bb8a21b2ff1391dd
inveker <alex.korolev.1995@gmail.com>
2023-09-28 13:55:55 +0300

    add nft royalty

--- contracts/core/tokens/NftToken.sol
+++ contracts/core/tokens/NftToken.sol
@@ -4,22 +4,33 @@ pragma solidity 0.8.18;
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import { ERC721Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import { IERC2981 } from "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import { IERC165 } from "@openzeppelin/contracts/interfaces/IERC165.sol";
 
 import { INftToken } from "../../interfaces/INftToken.sol";
 import { IAddressBook } from "../../interfaces/IAddressBook.sol";
 import { INftTokensObserver } from "../../interfaces/INftTokensObserver.sol";
 
-contract NftToken is INftToken, ReentrancyGuardUpgradeable, UUPSUpgradeable, ERC721Upgradeable {
+contract NftToken is
+    INftToken,
+    IERC2981,
+    ReentrancyGuardUpgradeable,
+    UUPSUpgradeable,
+    ERC721Upgradeable
+{
     address public addressBook;
+    uint256 public royaltyFees;
     uint256 public nextTokenId;
 
     function initialize(
         address _addressBook,
         string calldata _name,
-        string calldata _symbol
+        string calldata _symbol,
+        uint256 _royaltyFees
     ) public initializer {
         __ERC721_init(_name, _symbol);
         addressBook = _addressBook;
+        royaltyFees = _royaltyFees;
     }
 
     function _authorizeUpgrade(address) internal view override {
@@ -40,5 +51,19 @@ contract NftToken is INftToken, ReentrancyGuardUpgradeable, UUPSUpgradeable, ERC
     ) internal override {
         address nftTokensObserver = IAddressBook(addressBook).nftTokensObserver();
         INftTokensObserver(nftTokensObserver).notify(_nftId, _from, _to);
+    }
+
+    function royaltyInfo(
+        uint256,
+        uint256 salePrice
+    ) external view returns (address receiver, uint256 royaltyAmount) {
+        receiver = IAddressBook(addressBook).treasury();
+        royaltyAmount = (salePrice * royaltyFees) / 10000;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721Upgradeable, IERC165) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
