7d65a3f15bfdb983cddecd689d0ef2fdeaacf29e
inveker <alex.korolev.1995@gmail.com>
2023-09-28 13:53:01 +0300

    remove unused

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,10 +1,7 @@
 import { ethers } from "ethers"
 import { UsersMarketplace__factory } from "../typechain-types";
 
-async function main() {
-  let {a} = await UsersMarketplace__factory.connect().sellInfo()
-  a.payToken_;
-}
+async function main() {}
 
 main().catch((error) => {
   console.error(error);
