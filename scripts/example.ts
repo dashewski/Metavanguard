5b1f30e0af66555c6a64183def3c04766b9a0e97
inveker <alex.korolev.1995@gmail.com>
2023-10-13 13:26:19 +0300

    offchain listen example

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,7 +1,32 @@
 import { ethers } from "ethers"
-import { UsersMarketplace__factory } from "../typechain-types";
+import { NftReferalStaking__factory, NftTokensObserver__factory, UsersMarketplace__factory } from "../typechain-types";
 
-async function main() {}
+async function main() {
+  let provider: ethers.providers.JsonRpcProvider
+
+  // Nft Referal Staking
+  let referalStakingAddress: string
+  const nftReferalStaking = NftReferalStaking__factory.connect(referalStakingAddress,provider)
+
+  nftReferalStaking.on(nftReferalStaking.filters.Stake(), (stakingId, owner, nftToken, nftId) => {
+    // New Referal staking
+  })
+  nftReferalStaking.on(nftReferalStaking.filters.Unstake(), (stakingId, owner, nftToken, nftId) => {
+    // Close Referal staking
+  })
+  nftReferalStaking.on(nftReferalStaking.filters.Transfer(), (from, to, nftId) => {
+    // Change staking owner
+  })
+
+  // Nft Tokens Observer
+  
+  let nftTokensObserverAddress: string
+  const nftTokensObserver = NftTokensObserver__factory.connect(nftTokensObserverAddress,provider)
+
+  nftTokensObserver.on(nftTokensObserver.filters.ChangeOwner(), (nftToken, nftId, from, to) => {
+
+  })
+}
 
 main().catch((error) => {
   console.error(error);
